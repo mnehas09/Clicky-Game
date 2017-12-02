1bc2246478b3d014a03b001d511a35f72e034faa
Michael Nehas <Michael Nehas>
2017-12-01 22:31:51 -0500

    react files

---
+++ my-app/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HelloBootstrap from "./components/bootstrap";
+const App = () => (
+  <div className="container">
+    <HelloBootstrap />
+  </div>
+);
+export default App;
