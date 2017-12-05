b839dc1191345ddb8331b319be2edad0dfe16822
Michael Nehas <Michael Nehas>
2017-12-04 20:23:35 -0500

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
-import HelloBootstrap from "./components/bootstrap";
-const App = () => (
-  <div className="container">
-    <HelloBootstrap />
-  </div>
-);
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import FriendCard from "./components/FriendCard";
+import friends from "./friends.json";
+
+
+class App extends Component {
+  state = {
+    friends
+  };
+
+render() {
+    return (
+      <Wrapper>
+      <Navbar />
+        {this.state.friends.map(friend => (
+          <FriendCard
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
 export default App;
---
+++ my-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+const Navbar = props =>
+    <nav className = "navbar navbar-default" >
+     <div className="container-fluid">
+      <div className="navbar-header">
+       <div className="navbar-brand" to="/">
+        <a href="/">Clicky Game!</a>
+        </div> 
+       </div>
+     </div>
+    </nav>;
+
+export default Navbar;
